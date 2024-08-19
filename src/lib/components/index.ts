6c7118047db1071f18da9b62d505124a98e3d66e
Kyle Hulvey <83362164+kbh1301@users.noreply.github.com>
2024-08-18 21:14:57 -0500

    Rename folder

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,8 +1,8 @@
 export { default as WindowTitleBar } from "$components/window-title-bar.svelte";
 export { default as SideMenu } from "$components/side-menu.svelte";
-export { default as DialogGuide } from "$components/code-mirror/dialog-guide.svelte";
-export { default as DialogLink } from "$components/code-mirror/dialog-link.svelte";
-export { default as CodeMirror } from "$components/code-mirror/code-mirror.svelte";
+export { default as DialogGuide } from "$root/lib/components/editor/dialog-guide.svelte";
+export { default as DialogLink } from "$root/lib/components/editor/dialog-link.svelte";
+export { default as CodeMirror } from "$root/lib/components/editor/code-mirror.svelte";
 export { default as SwitchEdit } from "$components/switch-edit-mode.svelte";
 
 export * as LightSwitch from "$components/light-switch";
